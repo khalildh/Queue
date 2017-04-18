bfb8e3289068a2023b5223e71e92e645277589db
khughes <hughes.khalil@gmail.com>
2017-04-18 16:21:54 +0100

    Added a working push method

--- QDemo.java
+++ QDemo.java
@@ -1,13 +1,24 @@
 class Queue {
 	private int[] arr;
+	private int arrSize;
+	// the i variable keeps track of the number of pushes, 
+	// j keeps track of the number of pops
 	private int i, j;
 
 	Queue(int size) {
 		arr = new int[size];
+		arrSize = arr.length;
 	}
 
 	public boolean push(int x) {
-		return true;
+		if (i < arrSize) {
+			arr[i] = x;
+			i += 1;
+
+			printer();
+			return true;
+		}
+		return false;
 	}
 
 	public int pop() {
@@ -18,6 +29,8 @@ class Queue {
 		for (int x: arr) {
 			System.out.print(x + " ");
 		}
+		System.out.println();
+		// System.out.println(arrSize);
 	}
 	
 }
@@ -27,5 +40,15 @@ class QDemo {
 		Queue array = new Queue(5); 
 
 		array.printer();
+
+		array.push(4);
+		array.push(5);
+		array.push(6);
+		array.push(7);
+		array.push(8);
+		array.push(9);
+
+
+		
 	}
 }
